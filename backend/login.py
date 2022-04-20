2240c647b2d3e27c69590d8efff34d5903e6ba93
N-C-codes <che.nj.75357@gmail.com>
2022-04-20 23:18:47 +0100

    Refactoring to split code into functions; adding ability to create table only if it isn't there and get log in time.

--- backend/login.py
+++ backend/login.py
@@ -1,32 +1,63 @@
 import sqlite3
+from datetime import datetime as dt
 
-# Connect to the database with the login info and create a cursor:
-connection = sqlite3.connect("database/login_info.db")
-cursor = connection.cursor()
+def connect_to_database():
+    """Connects to the database where the log in information is held."""
 
-# Creating a table in the database:
-#cursor.execute("""CREATE TABLE login_data
-#(username TEXT, password TEXT)""")
+    return sqlite3.connect("database/login_info.db")
 
-# Add login value to database and save:
-#cursor.execute("""INSERT INTO login_data
-#VALUES (?,?)""", ['user123', 'pa55w0rd'])
-#connection.commit()
+def get_cursor(connection):
+    """Creates a cursor for the database connection."""
 
-user_attempt = input("Username: ")
-password_attempt = input("Password: ")
+    return connection.cursor()
 
-# Checking credentials given are valid:
-check = cursor.execute("""SELECT username, password FROM login_data where username=?""", [user_attempt])
-results = cursor.fetchall()
-if len(results) > 0:
-    print("User found.")
-    password = results[0][1]
-    if password_attempt == password:
-        print("Login successful.")
+def table_exists(cursor):
+    """Checks whether the login_data table already exists or not."""
+
+    cursor.execute("""SELECT name FROM sqlite_master WHERE type='table' AND name='login_data'""")
+    result = cursor.fetchone()
+
+    return True if len(result) > 0 else False
+
+def create_table(connection, cursor):
+    """Creates a table in the database for the login info and populates it."""
+    
+    # Create table and define the fields:
+    cursor.execute("""CREATE TABLE login_data
+    (username TEXT, password TEXT)""")
+
+    #Add valid user login to database and save:
+    cursor.execute("""INSERT INTO login_data
+    VALUES (?,?)""", ["user123", "pa55w0rd"])
+    connection.commit()
+
+if __name__ == "__main__":
+
+    # Connect to the database and get the cursor:
+    connection = connect_to_database()
+    cursor = get_cursor(connection)
+
+    # Check if there are any tables in the database and if not create one:
+    if not table_exists(cursor):
+        create_table(connection, cursor)
+
+    user_attempt = input("Username: ")
+    password_attempt = input("Password: ")
+
+    # Checking credentials given are valid:
+    cursor.execute("""SELECT username, password FROM login_data where username=?""", [user_attempt])
+    results = cursor.fetchall()
+    if len(results) > 0:
+        print("User found.")
+        password = results[0][1]
+        if password_attempt == password:
+            print("Login successful.")
+            # Get current time, format into <day> <month> <year> <hh:mm:ss>:
+            current_time = dt.now().strftime("%d %b %Y %H:%M:%S") 
+            print(f"Logged in at {current_time}.")
+        else:
+            print("Login failed: incorrect password.")
     else:
-        print("Login failed: incorrect password.")
-else:
-    # Deal with unique user problem elsewhere and assume there each user is unique:
-    print("Login failed: user not found.")
+        # Deal with unique user problem elsewhere and assume there each user is unique:
+        print("Login failed: user not found.")
 
