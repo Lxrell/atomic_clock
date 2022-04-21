7303a044d2c5fad72e8eb1f9ab16208cf7017623
N-C-codes <che.nj.75357@gmail.com>
2022-04-21 12:09:07 +0100

    Refactoring to make the program object oriented and only display time if logged in

--- backend/login_validator.py
+++ backend/login_validator.py
@@ -2,102 +2,139 @@ import ntplib
 import sqlite3
 from time import ctime
 
-def get_time_from_server():
-    """Gets the current time from the time server using NTP."""
-    response = c.request('europe.pool.ntp.org', version=3)
-    
-    return ctime(response.tx_time)
+class Login():
 
-def connect_to_database():
-    """Connects to the database where the log in information is held."""
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.logged_in = False
 
-    return sqlite3.connect("data/login_info_database.db")
+    def get_time_from_server(self):
+        """Gets the current time from the time server using NTP."""
+        
+        # Open connection to time server and get time:
+        c = ntplib.NTPClient()
+        response = c.request('europe.pool.ntp.org', version=3)
+        
+        return ctime(response.tx_time)
 
-def get_cursor(connection):
-    """Creates a cursor for the database connection."""
+    def connect_to_database(self):
+        """Connects to the database where the log in information is held."""
 
-    return connection.cursor()
+        return sqlite3.connect("data/login_info_database.db")
 
-def table_exists(cursor):
-    """Checks whether the login_data table already exists or not."""
+    def get_cursor(self, connection):
+        """Creates a cursor for the database connection."""
 
-    cursor.execute("""SELECT name FROM sqlite_master WHERE type='table' AND name='login_data'""")
-    result = cursor.fetchone()
+        self.connection = connection
 
-    return True if len(result) > 0 else False
+        return self.connection.cursor()
 
-def create_table(connection, cursor):
-    """Creates a table in the database for the login info and populates it."""
-    
-    # Create table and define the fields:
-    cursor.execute("""CREATE TABLE login_data
-    (username TEXT, password TEXT)""")
+    def table_exists(self, cursor):
+        """Checks whether the login_data table already exists or not."""
 
-    #Add valid user login to database and save:
-    cursor.execute("""INSERT INTO login_data
-    VALUES (?,?)""", ["user123", "pa55w0rd"])
-    connection.commit()
+        self.cursor = cursor
 
-def get_last_login_date():
-    """Gets the last login date from the last_login.txt file."""
+        self.cursor.execute("""SELECT name FROM sqlite_master WHERE type='table' AND name='login_data'""")
+        result = self.cursor.fetchone()
 
-    with open("data/last_login.txt", "r") as file:
-        content = file.readlines()
-        # For a first time login, there won't be a date, but there will be one for subsequent logins:
-        if len(content) > 0:
-            date = content[0]
+        return True if len(result) > 0 else False
+
+    def create_table(self, connection, cursor):
+        """Creates a table in the database for the login info and populates it."""
+
+        self.connection = connection
+        self.cursor = cursor
+        
+        # Create table and define the fields:
+        self.cursor.execute("""CREATE TABLE login_data
+        (username TEXT, password TEXT)""")
+
+        #Add valid user login to database and save:
+        self.cursor.execute("""INSERT INTO login_data
+        VALUES (?,?)""", ["user123", "pa55w0rd"])
+        self.connection.commit()
+
+    def get_last_login_date(self):
+        """Gets the last login date from the last_login.txt file."""
+
+        with open("data/last_login.txt", "r") as file:
+            content = file.readlines()
+            # For a first time login, there won't be a date, but there will be one for subsequent logins:
+            if len(content) > 0:
+                date = content[0]
+            else:
+                # First login case - there is no date:
+                date = ""
+        
+        return date
+
+    def set_last_login_date(self, date):
+        """Writes the last login date into the last_login.txt file, overriding the previous date present."""
+
+        self.date = date
+
+        with open("data/last_login.txt", "w") as file:
+            file.write(self.date)
+
+    def login_attempt(self, cursor, user_attempt, password_attempt):
+        """Checks the given credentials match those in the database; if login successful will print last login date and set last login date for next time."""
+        
+        self.cursor = cursor
+        self.user_attempt = user_attempt
+        self.password_attempt = password_attempt
+        
+        self.cursor.execute("""SELECT username, password FROM login_data where username=?""", [self.user_attempt])
+        results = self.cursor.fetchall()
+        if len(results) > 0:
+            # I am sssuming each user is unique (only created 1 in database!):
+            # results is a list of tuples (username, password):
+            password = results[0][1]
+            if self.password_attempt == password:
+                # Get current login time, using the time server:
+                current_login_time = self.get_time_from_server()
+                last_login = self.get_last_login_date() 
+                self.set_last_login_date(current_login_time)
+                self.logged_in = True
+                return f"Login successful. Last logged in at {last_login}." # *Needs to be sent to frontend*   
+            else:
+                return "Login failed: incorrect password."
         else:
-            # First login case - there is no date:
-            date = ""
-    
-    return date
-
-def set_last_login_date(date):
-    """Writes the last login date into the last_login.txt file, overriding the previous date present."""
-
-    with open("data/last_login.txt", "w") as file:
-        file.write(date)
-
-def login_attempt(cursor, user_attempt, password_attempt):
-    """Checks the given credentials match those in the database; if login successful will print last login date and set last login date for next time."""
-    cursor.execute("""SELECT username, password FROM login_data where username=?""", [user_attempt])
-    results = cursor.fetchall()
-    if len(results) > 0:
-        # I am sssuming each user is unique (only created 1 in database!):
-        # results is a list of tuples (username, password):
-        password = results[0][1]
-        if password_attempt == password:
-            # Get current login time, using the time server:
-            current_login_time = get_time_from_server()
-            last_login = get_last_login_date() 
-            set_last_login_date(current_login_time)
-            return f"Login successful. Last logged in at {last_login}." # *Needs to be sent to frontend*   
-        else:
-            return "Login failed: incorrect password."
-    else:
-        return "Login failed: user not found."
+            return "Login failed: user not found."
 
 if __name__ == "__main__":
-
-    # Displaying current time using a time server:
-    # Open connection to server:
-    c = ntplib.NTPClient()
-    current_time = get_time_from_server()
-    print(current_time) # *Needs to be sent to frontend*
-
-    # Connect to the database and get the cursor:
-    connection = connect_to_database()
-    cursor = get_cursor(connection)
-
-    # Check if there are any tables in the database and if not create one:
-    if not table_exists(cursor):
-        create_table(connection, cursor)
 
     # Using user input to simulate getting login details from frontend:
     user_attempt = input("Username: ") # *Needs to be retrieved from frontend*
     password_attempt = input("Password: ") # *Needs to be retrieved from frontend*
 
+    # Create a new login instance for when someone tries to log in:
+    new_login = Login(user_attempt, password_attempt)
+
+    # Connect to the database and get the cursor:
+    connection = new_login.connect_to_database()
+    cursor = new_login.get_cursor(connection)
+
+    # Check if there are any tables in the database and if not create one:
+    if not new_login.table_exists(cursor):
+        new_login.create_table(connection, cursor)
+
     # Attempt the login with the given credentials:
-    result = login_attempt(cursor, user_attempt, password_attempt)
+    result = new_login.login_attempt(cursor, user_attempt, password_attempt)
     print(result) # *Needs to be sent to frontend*
+
+    # Displaying current time using a time server if we are logged in:
+    if new_login.logged_in == True:
+        # Time server request may time out and raise an exception:
+        try:
+            # Get and display current time from server:
+            current_time = new_login.get_time_from_server()
+            print(f"The current time is {current_time}.") # *Needs to be sent to frontend*
+        except ntplib.NTPException:
+            print("No response was received from the server.")
+
+
+    
+
+    
 
