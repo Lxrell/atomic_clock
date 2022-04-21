d029c2a71103b5830e5cba26b3a183b99e71d659
Lxrell <70538102+Lxrell@users.noreply.github.com>
2022-04-21 10:05:50 +0100

    new

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+$(document).ready(function(){
+    
+    atomic_clock();
+    $('#login_button').click(function(){
+        if('#'){
+            location.replace("/main.html");
+        }else{
 
+        }
+        
+    })
+
+    function atomic_clock(){
+        ntpClient.getNetworkTime("pool.ntp.org", 123, function(err, date) {
+            if(err) {
+                console.error(err);
+                return;
+            }
+         
+            console.log("Current time : ");
+            console.log(date); // Mon Jul 08 2013 21:31:31 GMT+0200 (Paris, Madrid (heure d’été))
+        });
+    }
+})
