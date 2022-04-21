e1dcd35a6147c966bf29cbdae41a22dc7aa23b6a
Lxrell <70538102+Lxrell@users.noreply.github.com>
2022-04-21 15:46:14 +0100

    goddamn js

--- frontend/script.js
+++ frontend/script.js
@@ -1,24 +1,37 @@
 $(document).ready(function(){
     
     atomic_clock();
-    $('#login_button').click(function(){
-        if('#'){
-            location.replace("/main.html");
-        }else{
 
-        }
+    // $('#login_button').click(function(){
+    //     if('#'){
+    //         location.replace("/main.html");
+    //     }else{
+
+    //     }
         
-    })
+    // });
+
+    // $('#atomic_clock').html(date());
 
     function atomic_clock(){
-        ntpClient.getNetworkTime("pool.ntp.org", 123, function(err, date) {
-            if(err) {
-                console.error(err);
-                return;
-            }
-         
-            console.log("Current time : ");
-            console.log(date); // Mon Jul 08 2013 21:31:31 GMT+0200 (Paris, Madrid (heure d’été))
+        const {spawn} = require('child_process');
+        const childPython=spawn('python',['./backend/login.py']);
+        childPython.stdout.on('data',(data)=>{
+        console.log(`Current Time ${data}`)
         });
     }
-})
+//     function atomic_clock(){
+
+//     $.ajax({
+//         type:'GET',
+//         dataType: 'JSON',
+//         url: './backend/login.py/get_time_from_server',
+//         success: function(data){
+//             console.log(data);
+//         }
+//     })
+// }
+
+    
+
+});
