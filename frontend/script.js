8b934aed54a62cd9364e067f6d766791f4fb9673
Lxrell <70538102+Lxrell@users.noreply.github.com>
2022-04-22 14:21:17 +0100

    First stage of if statement for login

--- frontend/script.js
+++ frontend/script.js
@@ -2,16 +2,25 @@ $(document).ready(function(){
     
     atomic_clock();
 
-    // $('#login_button').click(function(){
-    //     if('#'){
-    //         location.replace("/main.html");
-    //     }else{
+    $('#login_button').click(function(){
+        if($('#username').val().length() == 0){
+            alert('Enter Username');
+        }else if($('#input_password').val().length() == 0){
+            alert('Enter Password');
 
-    //     }
+        }else{
+            /*Enter code to get data from backend
+            if returned is true then open main page
+            else alert fail, incorrect username/ password etc
+            */
+        }
+
+
+        location.replace("/main.html");
         
-    // });
+    });
 
-    // $('#atomic_clock').html(date());
+    $('#atomic_clock').html(date());
 
     function atomic_clock(){
         const {spawn} = require('child_process');
@@ -19,19 +28,6 @@ $(document).ready(function(){
         childPython.stdout.on('data',(data)=>{
         console.log(`Current Time ${data}`)
         });
-    }
-//     function atomic_clock(){
-
-//     $.ajax({
-//         type:'GET',
-//         dataType: 'JSON',
-//         url: './backend/login.py/get_time_from_server',
-//         success: function(data){
-//             console.log(data);
-//         }
-//     })
-// }
-
-    
+    }    
 
 });
